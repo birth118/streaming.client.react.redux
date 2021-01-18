e32ec06af5099bbcb33ff557b1d99e34d06a7918
Seongsoo Yim <seongsooyim@192-168-1-100.tpgi.com.au>
2021-01-18 11:12:25 +1100

    <video /> added

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react'
+import flvjs from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends Component {
 
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id)
+  constructor(props) {
+    super(props)
+
+    this.videoRef = React.createRef()
+
   }
 
+  componentDidMount() {
+
+    const { id } = this.props.match.params
+    this.props.fetchStream(id)
+
+    // 3rd party APU fetch usually goes to componentDidMount()
+    this.buildPlayer()
+    // this.flvPlayer.play();
+  }
+
+  componentDidUpdate() {
+
+    // When componentDidMount() called, <video /> elememt is not rendered yet 
+    // because it was returned with 'Loading...'
+    // So, buildPlayer() called in componentDidUpdate()
+
+    this.buildPlayer()
+  }
+
+  componentWillUnmount() {
+
+    console.log('Player Unmounted');
+    this.flvPlayer.destroy()
+  }
+
+
+  buildPlayer() {
+    if (this.flvPlayer || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params
+
+    // 3rd party API fetch usually goes to componentDidMount()
+
+    this.flvPlayer = flvjs.createPlayer({
+      type: 'flv',
+      //url: `http://localhost:8000/live/${id}`
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
+
+    //   this.flvPlayer.play();
+
+
+  }
 
   render() {
     if (!this.props.stream) {
@@ -16,7 +67,9 @@ class StreamShow extends Component {
     const { title, description } = this.props.stream
     return (
       < div >
-        \        <div className="ui content">
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
+        {/* React videio player component */}
+        <div className="ui content">
           <div className="ui header">{title}</div>
           <div className="ui description">{description}</div>
 
